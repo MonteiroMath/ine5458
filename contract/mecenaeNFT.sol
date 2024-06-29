c4a77613503938f452055cfd98d1ce54d5f21e4a
Matheus Monteiro <math.a.monteiro@gmail.com>
2024-06-29 19:15:02 -0300

    final version of smart contract

--- contract/mecenaeNFT.sol
+++ contract/mecenaeNFT.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 
-contract MecenaeNFT is ERC721URIStorage {
+contract FinalMecenaeNFT is ERC721URIStorage {
    
     uint256 private _tokenIds;
     mapping(uint256 => uint256) public tokenPrice;
@@ -12,21 +12,20 @@ contract MecenaeNFT is ERC721URIStorage {
 
     constructor() ERC721("MecenaeNFT2", "MCN") {}
 
-    function mintNFT(address recipient, string memory name, string memory description, string memory image)
+    function mintNFT(address recipient, string memory name, string memory description, string memory image, string memory eventName, string memory eventDate, string memory ngoName)
         public returns (uint256)
     {
         _tokenIds += 1;
         uint256 newItemId = _tokenIds;
         _mint(recipient, newItemId);
-        _setTokenURI(newItemId, encodeMetadata(name, description, image));
-
+        _setTokenURI(newItemId, encodeMetadata(name, description, image, eventName, eventDate, ngoName));
+    
         return newItemId;
     }
-
-    function encodeMetadata(string memory name, string memory description, string memory image)
+    
+    function encodeMetadata(string memory name, string memory description, string memory image, string memory eventName, string memory eventDate, string memory ngoName)
         internal pure returns (string memory)
     {
-        // Encode the metadata into a JSON format or use a service like IPFS to store metadata
         return string(
             abi.encodePacked(
                 '{"name":"',
@@ -37,10 +36,20 @@ contract MecenaeNFT is ERC721URIStorage {
                 '",',
                 '"image":"',
                 image,
+                '",',
+                '"eventName":"',
+                eventName,
+                '",',
+                '"eventDate":"',
+                eventDate,
+                '",',
+                '"ngoName":"',
+                ngoName,
                 '"}'
             )
         );
     }
+
 
     function buyNFT(uint256 tokenId) public payable {
         require(tokenPrice[tokenId] > 0, "This token is not for sale.");
@@ -48,20 +57,21 @@ contract MecenaeNFT is ERC721URIStorage {
 
         address seller = ownerOf(tokenId);
 
-        uint256 transactionFee = (tokenPrice[tokenId] * 1) / 100; // Calculate 1% transaction fee
+        uint256 transactionFee = (tokenPrice[tokenId] * 1) / 100;
         uint256 remainingValue = tokenPrice[tokenId] - transactionFee;
 
-        // Transfer transaction fee to the fixed wallet address (replace with your actual wallet address)
+       
         address feeWallet = 0x91370da2633e5b633588376ab25CdaD93227dc8C;
         payable(feeWallet).transfer(transactionFee);
 
         _transfer(seller, msg.sender, tokenId);
         payable(seller).transfer(remainingValue);
-        tokenPrice[tokenId] = 0; // Token is no longer for sale
+        tokenPrice[tokenId] = 0; 
     }
 
     function sellNFT(uint256 tokenId, uint256 price) public {
         require(ownerOf(tokenId) == msg.sender, "You are not the owner.");
         tokenPrice[tokenId] = price;
     }
+
 }
